b81493fc33b9c4792a64a8a5c5d882f5d86ee4ab
spectralworx <pavle.ciric.dev@gmail.com>
2026-02-20 21:58:25 +0100

    Sitne promene

--- database/seeders/CallCenterUserSeeder.php
+++ database/seeders/CallCenterUserSeeder.php
@@ -2,20 +2,22 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class CallCenterUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::query()->updateOrCreate(
+        User::updateOrCreate(
             ['email' => 'callcenter@local'],
             [
                 'name' => 'Call Center',
-                'password' => Hash::make(str()->random(40)), // ne koristi se, samo da nije null
-                'pin_hash' => Hash::make(env('CALLCENTER_PIN', '2026')),
+                'pin_hash' => Hash::make('2026'),
+                // password može ostati null ako si ga izbacio iz auth flow-a
+                // ako users tabela zahteva password NOT NULL, onda stavi random:
+                // 'password' => Hash::make(str()->random(32)),
             ]
         );
     }
